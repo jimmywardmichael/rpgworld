2a13e646d2b8f399527607ca5aae6d219ff6c965
jimmywardmichael <jimmywardmichael@gmail.com>
2025-05-16 09:07:57 -0500

    init

---
+++ RPGWORLD/src/test/java/com/rpg/RPGWORLD/RpgworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.RPGWORLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
